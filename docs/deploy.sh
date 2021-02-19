1640ce88a30b28739bc9644b642262b2e3d1d733
Dominic Cartwright <dominic.cartwright07@gmail.com>
2021-02-19 15:12:11 -0500

    Update deploy.sh

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:WhatsUp247/wu247docs.git main:gh-pages
+git push -f git@github.com:WhatsUp247/wu247docs.git master:gh-pages
 
 cd -
